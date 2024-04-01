52d44794a0e9a29e6412013fe82ea548b66c5673
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-04-01 15:05:14 +0300

    Initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const index = () => {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-[#32cd32]">
+      <Text className="text-[#ffd700] text-[17px]">
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+};
+
+export default index;
